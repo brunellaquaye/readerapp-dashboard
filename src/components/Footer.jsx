0fe726067334521548ee564eb03bb4ef98a8b9a4
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-07-31 16:33:38 +0000

    feat: Implement footer of dashboard.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer flex items-center justify-between p-[30px] ">
+    <span className="font-bold">READERAPP</span>
+    <span className="font-bold text-[14px] hover:bg-red-400">Readerapp admin Dashboard</span>
+    </div>
   )
 }
 
